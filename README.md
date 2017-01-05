7d0e358c7c48162148073696ee154242f80119c8
norybiak <norybiak@gmail.com>
2017-01-04 23:38:17 -0800

    v0.1.2 Init Commit

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # UltimateLoader
+A tool to load objects of various types for Three.js in AltspaceVR
+
+## Files types
+Currently accepted object file types:
+* .obj
+* .json
+* .dae
+* .gltf
+
+Currently accepted image file types (will be mapped to plane):
+* .jpeg / .jpg
+* .png
+
+**IMPORTANT:** Your objects aren't guaranteed  to load correctly. Some models may not have been converted correctly or weren't setup right.
+
+Currently accepted textures file types:
+* .jpeg / .jpg
+* .png
+
+**IMPORTANT:** Your texture must be named that same as the object file and placed in the same directory.
+
+## Using UltimateLoader
+
+There are two methods you can use to load objects. 
+* UltimateLoader.load(path, callback);
+* UltimateLoader.multiload(arrayOfPaths, callback);
+
+Each method functions the same except that multiloader will return an array of references to all the loaded objects in order of urls provided.
+
+If you need to imcrementally add objects, you can set UltimateLoader.queue to true. This will put all objects into a queue and load them one at a time which slows the entire loading process down. It's recommended to keep it false.
+
+### UltimateLoader.load(path, callback);
+```javascript
+UltimateLoader.load('path/to/model', function(object)
+{
+  // Do work...
+  
+  //Example work
+  object.position.set(x, y, z);
+  scene.add(object);
+  
+  // Do more work...
+});
+```
+
+
+
+### UltimateLoader.multiload(arrayOfPaths, callback);
+```javascript
+var objectUrls =
+[
+	'/model/object.dae',
+	'/model/object.obj',
+	...
+];
+
+var loadedObject = [];
+
+UltimateLoader.multiload(objectUrls, function(objects)
+{
+
+  for (var i = 0; i < objects.length; i++)
+  {
+  	objects[i].position.set(0,-450,0);
+  	objects[i].scale.set(5,5,5);
+  
+  	scene.add(objects[i]);
+  	
+  	loadedObjects[i] = objects[i];
+  }
+  
+});
+```
---
+++ src/UltimateLoader.js
@@ -0,0 +1,400 @@
+/**
+ * ULTIMATE LOADER
+ * A tool to help load objects in Three.js (for Altspace)
+ * 
+ * @Author NorybiaK
+ * version 1.2
+ */
+
+var UltimateLoader = UltimateLoader || {};
+
+(function(main) 
+{ 'use strict';
+
+	//A list containing all of the objects loaded by file name (used for cloning)
+	var listOfObjectFilesLoaded = {};
+
+	var queueList = [];
+	var next = 0;
+	var lastStop;
+	
+	var crossOrigin = 'anonymous';
+	var baseUrl = '';
+	
+   /** 
+	* If true, the objects will load incrementally. Loading time is drastically increased.
+	*
+    */
+	main.queue = false;
+	
+   /** 
+	* Default size is 32 (arbitrary number that works)
+	* Depending on enclosure size, this number will need to be changed.
+	* 
+    */
+	
+	main.imageSize = 32;
+
+   /** 
+	*	UltimateLoader.load()
+	*	Load an object and then run the callback function.
+	* 
+	*	The object is returned via the callback.
+    */
+	main.load = function(objUrl, callback)
+	{
+		if (main.queue)
+		{
+			queue(objUrl, callback);
+			
+			//checks if the newly queued object is next
+			//start the queue if it is
+			start();
+		}
+		else
+		{
+			var arr = [objUrl, callback];
+			
+			load(arr);
+		}
+	}
+	
+   /** 
+	*	UltimateLoader.multiload()
+	*	Load an array of objects and run the callback function.
+	*
+	* 	An array of objects is returned (in order of the objects passed in) via the callback.
+    */
+	main.multiload = function(objUrls, callback)
+	{
+		var objectsLoaded = [];
+		var totalLoaded = 0;
+		
+		var callbackFunction = function(file)
+		{
+			objectsLoaded[file.i] = file.object;
+			totalLoaded++;
+			
+			if (totalLoaded == objUrls.length)
+			{
+				callback(objectsLoaded);
+			}
+		};
+		
+		for (var i = 0; i < objUrls.length; i++)
+		{
+			if (main.queue)
+			{
+				queue(objUrls[i], callbackFunction, i);
+				
+				//checks if the newly queued object is next
+				//start the queue if it is
+				start();
+			}
+			else
+			{
+				var arr = [objUrls[i], callbackFunction, i];
+			
+				load(arr);
+			}
+		}
+		
+		//checks if the newly queued object is next
+		//start the queue if it is
+		start();
+	}
+	
+   /** 
+	*	queue()
+	*	Add object reference to the queue list.
+	*
+    */
+	function queue(objUrl, callback)
+	{
+		queueList.push([objUrl, callback]);
+	}
+
+   /** 
+	*	dequeue()
+	*	Load the next object in the queue.
+	*
+    */
+	function dequeue()
+	{
+		//Nothing to get! Empty queueList
+		if (next > queueList.length-1) 
+		{ 
+			queueList = [];
+			next = 0;
+			return;
+		}
+	
+		var object = queueList[next];
+		next++;
+		
+		load(object);
+	}
+	
+   /** 
+	*	start()
+	*	Helper function to determine if we need to dequeue.
+	*
+    */
+	function start()
+	{
+		if (next == 0)
+		{
+			dequeue();
+		}
+	}
+	
+   /** 
+	*	load()
+	*	Check the object file extension and use the correct loader.
+	*	If the object file already exists, clone in.
+	*
+    */
+	function load(arr)
+	{
+		var url = arr[0];
+		var callback = arr[1];
+		
+		var file = getFileInfo(url);
+		file.url = url;
+		file.callback = callback;
+		
+		//Special case for multiload.
+		if (arr[2] != null)
+		{
+			file.i = arr[2];
+		}
+	
+		switch (file.ext)
+		{
+			case "obj":
+				loadOBJ(file);
+				break;
+				
+			case "json":
+				loadJSON(file);
+				break;
+				
+			case "dae":
+				loadCollada(file);
+				break;
+				
+			case "gltf":
+				loadglTF(file);
+				break;
+				
+			case "fbx":
+				loadFBX(file);
+				break;
+				
+			case "png":
+				loadImage(file);
+				break;
+				
+			case "jpg":
+				loadImage(file);
+				break;
+				
+			case "jpeg":
+				loadImage(file);
+				break;
+				
+			default:
+				console.log("UltimateLoader: File extension -" + file.ext + "- not recognized! Object -" + file.name + "- did not load.");
+				dequeue(); //Move to the next object
+				break;
+		}
+		
+	}
+	
+   /** 
+	*	getFileInfo()
+	*	Sets the baseUrl and splits the filename into an object called info. 
+	*	Info contains the name, extention, and the baseUrl.
+	*
+    */
+	function getFileInfo(path)
+	{
+		var newPath = path.slice(0, path.lastIndexOf('/') + 1);
+		if (newPath !== "" || newPath !== null)
+		{
+			baseUrl = newPath;
+		}
+		
+		var file = path.substr(path.lastIndexOf('/') + 1);
+		
+		var s = file.split('.');
+
+		var info = {name: s[0], ext: s[s.length-1].toLowerCase(), baseUrl: newPath};
+		
+		return info;	
+	}
+
+   /** 
+	*	loadOBJ()
+	*	.obj file found, attempt to load the .obj and .mtl.
+	*
+    */
+	function loadOBJ(file)
+	{
+		var obj = file.name + '.obj';
+		var mtl = file.name + '.mtl';
+		
+		var mtlLoader = new THREE.MTLLoader();
+		
+		mtlLoader.setPath(file.baseUrl);
+		mtlLoader.setTexturePath ? mtlLoader.setTexturePath(file.baseUrl) : mtlLoader.setBaseUrl(file.baseUrl);
+		mtlLoader.setCrossOrigin(crossOrigin);
+		
+		mtlLoader.load(mtl, function(materials) 
+		{
+			materials.preload();
+
+			var objLoader = new THREE.OBJLoader();
+			
+			objLoader.setMaterials(materials);
+			objLoader.setPath(file.baseUrl);
+
+			objLoader.load(obj, function (object)
+			{
+
+				object.traverse(function(child) 
+				{
+                  if (child instanceof THREE.Mesh) 
+				  {
+                      child.material.side = THREE.DoubleSide;
+                  }
+				});
+
+				file.object = object;
+				handleOnLoad(file);
+			}, onProgress, onError);
+		});	
+	}
+
+   /** 
+	*	loadJSON()
+	*	.json file found, attempt to load it.
+	*
+    */
+	function loadJSON(file)
+	{
+		var loader = new THREE.ObjectLoader();
+		
+		loader.load(file.url, function (object) 
+		{
+			file.object = object;
+			handleOnLoad(file);
+		}, onProgress, onError);
+	}
+	
+   /** 
+	*	loadCollada()
+	*	.dae file found, attempt to load it.
+	*
+    */
+	function loadCollada(file)
+	{
+		var loader = new THREE.ColladaLoader();
+		
+		loader.load(file.url, function (collada) 
+		{
+			var object = collada.scene;
+			
+			file.object = object;
+			handleOnLoad(file);
+		}, onProgress, onError);
+	}
+	
+   /** 
+	*	loadgltf()
+	*	.gltf file found, attempt to load it.
+	*	This one is iffy and doesn't have full proper implementaion. May not work properly.
+	*
+    */
+	function loadglTF(file)
+	{
+		var loader = new THREE.glTFLoader();
+		
+		loader.load(file.url, function(gltf) 
+		{
+			var object = gltf.scene;
+			
+			file.object = object;
+			handleOnLoad(file);
+		}, onProgress, onError);
+		
+	}
+	
+   /** 
+	*	loadImage()
+	*	.png or .jpeg file found, attempt to load it.
+	*	Adds texture to a plane. User may change the default plane transform via callback.
+	*
+    */
+	function loadImage(file)
+	{
+		var loader = new THREE.TextureLoader();
+		
+		loader.load(file.url, function(texture) 
+		{
+			file.object = texture;
+			handleOnLoad(file);
+		}, onProgress, onError);
+	}
+	
+	
+	function loadFBX(file)
+	{
+	
+		var loader = new THREE.FBXLoader();
+		
+		loader.load(file.url, function(object) 
+		{
+			file.object = object;
+			handleOnLoad(file);
+		}, onProgress, onError() );
+		
+	}
+	
+	function onProgress(xhr) 
+	{
+
+	}
+
+	function onError(xhr) 
+	{ 
+		console.log("There was an error loading your object");	
+	}
+
+   /** 
+	*	handleOnLoad()
+	*	Add the object into the loaded array and run the callback.
+	*
+    */
+	function handleOnLoad(file)
+	{
+		listOfObjectFilesLoaded[file.name] = file.object;
+
+		if (file.i != null)
+		{
+			file.callback(file);
+		}
+		else
+		{
+			file.callback(file.object);
+		}
+		
+		if (main.queue)
+		{
+			dequeue();
+		}	
+		
+		console.log("UltimateLoader: Object " + file.name + " loaded!");
+	}
+	
+})(UltimateLoader);
